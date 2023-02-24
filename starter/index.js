bed575fc71d8eefae526667737d4ffb024cbf121
Computastar <jason.hunter@computastar.com>
2023-02-24 15:44:16 +0000

    Created getRole function

--- starter/index.js
+++ starter/index.js
@@ -17,4 +17,40 @@ const roles = require('./lib/role.json');
 /*-- Global Variables --*/
 const employee = [];
 
+/* function to prompt for role or quit */
+const getRole = async() => {
 
+    const result = [];
+
+    
+    for (role of roles.roles)
+    {
+         result.push(role.role)
+    }
+
+    result.push("End Program")
+
+    const getRole = await inquirer.prompt([
+    {
+        type: 'list',
+        message: `What role would you like to add? `,
+        name: 'role',
+        choices: result
+    },
+]);
+
+if (getRole.role === "End Program")
+{
+    return
+}
+//askAndCreate(getRole.role.toLowerCase());
+}
+
+
+/*-- Initiate program --*/
+
+const init = () => {
+    getRole();
+}
+
+init();
